aceba37ca5019f6d046ba444ea71365b88fa68f5
Miguel110 <74572107+Miguel110@users.noreply.github.com>
2020-11-16 17:04:45 -0600

    Add files via upload

---
+++ CasiPorti/CasiPorti/main.js
@@ -0,0 +1,9 @@
+document.querySelector(".arti").addEventListener("click", () => {
+  document.querySelector(".arti").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 800 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 800 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 800 });
+
